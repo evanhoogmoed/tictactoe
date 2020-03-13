6c1008307430019624e8603afdcd44a7edec6e72
evanhoogmoed <56660574+evanhoogmoed@users.noreply.github.com>
2020-03-12 20:57:57 -0700

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,83 +1,184 @@
-import copy
-import time
+import queue
+import random
+import math
 
-class Game(object):
-    """A tic-tac-toe game."""
+class Board:
 
-    def __init__(self, grid):
-        """Instances differ by their grid marks."""
-        self.grid = copy.deepcopy(grid) # No aliasing!
+    def __init__(self):
+        #self.board = [list(range(1, 5)), list(range(5, 9)), list(range(9, 13)), list(range(13, 16)) + ['*']]
+        N = input("Input a number for the size of the board")
+        N = int(N)
+        size_of_board = int(math.sqrt(N+1))
+        self.board = []
+        begin = 1
+        tempValueHolder =0
 
-    def display(self):
-        """Print the game board."""
-        for row in self.grid:
-            print(row)
+        for i in range(1,size_of_board+1):
+          row = [list(range(begin,size_of_board + 1))]
+          self.board.append(row)
+          #begin = size_of_board+1
+        #self.board = [list()]
+        #self.board = [list()]
 
-    def moves(self):
-        """Return a list of possible moves given the current marks."""
-        for row in self.grid:
-          if self.grid == '-':
-            possible_moves = [self.grid]
-        return possible_moves
-        #Basically, check which space has - and return that this is a
-        #a possible move. We will use it afterwards with minimax
-        #returns the pair of possible locations
+        self.goal = []
+        for i in self.board:
+            self.goal.append(tuple(i))
+        self.goal = tuple(self.goal)
+        self.empty = [3, 3]
 
-    def neighbor(self, move, mark):
-        """Return a Game instance like this one but with one move made."""
+    def __repr__(self):
+        string = ''
+        for row in self.board:
+            for num in row:
+                if len(str(num)) == 1:
+                    string += '   ' + str(num)
+                elif len(str(num)) > 1:
+                    string += '  ' + str(num)
+            string += '\n'
+        return string
+
+    def convert_to_tuple(self, ar):
+        result = []
+        for i in ar:
+            result.append(tuple(i))
+        return tuple(result)
+
+    def convert_to_list(self, tup):
+        result = []
+        for i in tup:
+            result.append(list(i))
+        return result
+
+    def match(self, copy):
+        a = Board()
+        a.board = copy
+        for row in range(0, 4):
+            for col in range(0, 4):
+                if a.board[row][col] == '*':
+                    a.empty = [row, col]
+        result = []
+        for i in a.board:
+            result.append(list(i))
+        a.board = result
+        return a
+
+    def move_up(self): # move empty block up
+        try:
+            if self.empty[0] != 0:
+                tmp = self.board[self.empty[0]-1][self.empty[1]]
+                self.board[self.empty[0]-1][self.empty[1]] = '*'
+                self.board[self.empty[0]][self.empty[1]] = tmp
+                self.empty = [self.empty[0]-1, self.empty[1]]
+        except IndexError:
+            pass
+
+    def move_down(self): # move empty block down
+        try:
+            tmp = self.board[self.empty[0]+1][self.empty[1]]
+            self.board[self.empty[0]+1][self.empty[1]] = '*'
+            self.board[self.empty[0]][self.empty[1]] = tmp
+            self.empty = [self.empty[0]+1, self.empty[1]]
+        except IndexError:
+            pass
+
+    def move_right(self): # move empty block right
+        try:
+            tmp = self.board[self.empty[0]][self.empty[1]+1]
+            self.board[self.empty[0]][self.empty[1]+1] = '*'
+            self.board[self.empty[0]][self.empty[1]] = tmp
+            self.empty = [self.empty[0], self.empty[1]+1]
+        except IndexError:
+            pass
+
+    def move_left(self): # move empty block left
+        try:
+            if self.empty[1] != 0:
+                tmp = self.board[self.empty[0]][self.empty[1]-1]
+                self.board[self.empty[0]][self.empty[1]-1] = '*'
+                self.board[self.empty[0]][self.empty[1]] = tmp
+                self.empty = [self.empty[0], self.empty[1]-1]
+        except IndexError:
+            pass
+
+    def shuffle(self, steps):
+        for i in range(0, steps):
+            direction = random.randrange(1, 5)
+            if direction == 1:
+                self.move_up()
+            elif direction == 2:
+                self.move_right()
+            elif direction == 3:
+                self.move_left()
+            elif direction == 4:
+                self.move_down()
+
+    def solve(self):
+        start = self.convert_to_tuple(self.board)
+        pred = {}
+        visited = []
+        frontier = queue.Queue()
+        frontier.put(start)
         
-        # YOU FILL THIS IN
+        while frontier.qsize() > 0:
+            tmp = frontier.get()
+            
+            if tmp == self.goal:
+                path = []
+                while tmp != start:
+                    path.append(pred[tmp][1])
+                    tmp = pred[tmp][0]
+                return path[::-1]
+            
+            if tmp not in visited:
+                visited.append(tmp)
+                tmpboard = self.match(tmp)
+                tmpboard.move_up()
+                if self.convert_to_tuple(tmpboard.board) != tmp:
+                    frontier.put(self.convert_to_tuple(tmpboard.board))
+                    if self.convert_to_tuple(tmpboard.board) not in pred:
+                        pred[self.convert_to_tuple(tmpboard.board)]=[tmp, 'up']
 
-    def utility(self):
-        """Return the minimax utility value of this game:
-        1 = X win, -1 = O win, 0 = tie, None = not over yet."""
-        # YOU FILL THIS IN
-        # for utility here, check the code from this link
-        #http://www.sarathlakshman.com/2011/04/29/writing-a-tic-tac
-        #to calculate the utility of this current board.
-       
+                
+                tmpboard = self.match(tmp)
+                tmpboard.move_down()
+                if self.convert_to_tuple(tmpboard.board) != tmp:
+                    frontier.put(self.convert_to_tuple(tmpboard.board))
+                    if self.convert_to_tuple(tmpboard.board) not in pred:
+                        pred[self.convert_to_tuple(tmpboard.board)]=[tmp, 'down']
 
-class Agent(object):
-    """Knows how to play tic-tac-toe."""
+                        
+                tmpboard = self.match(tmp)
+                tmpboard.move_right()
+                if self.convert_to_tuple(tmpboard.board) != tmp:
+                    frontier.put(self.convert_to_tuple(tmpboard.board))
+                    if self.convert_to_tuple(tmpboard.board) not in pred:
+                        pred[self.convert_to_tuple(tmpboard.board)]=[tmp, 'right']
 
-    def __init__(self, mark):
-        """Agents use either X or O."""
-        self.mark = mark
+                
+                tmpboard = self.match(tmp)
+                tmpboard.move_left()
+                if self.convert_to_tuple(tmpboard.board) != tmp:
+                    frontier.put(self.convert_to_tuple(tmpboard.board))
+                    if self.convert_to_tuple(tmpboard.board) not in pred:
+                        pred[self.convert_to_tuple(tmpboard.board)]=[tmp, 'left']
 
-    def maxvalue(self, game, opponent):
-        """Compute the highest utility this game can have."""
-        # YOU FILL THIS IN
+        raise Exception('There is no solution.')
 
-    def minvalue(self, game, opponent):
-        """Compute the lowest utility this game can have."""
-        # YOU FILL THIS IN
 
-def main():
-    """Create a game and have two agents play it."""
+board = Board()
+board.shuffle(20)
+print (board)
+path = board.solve()
+print (path)
 
-    game = Game([['-','-','-'], ['-','-','-'], ['-','-','-']])
-    game.display()
+for dir in path:
+    if dir == 'right':
+        board.move_right()
+    if dir == 'left': 
+        board.move_left()
+    if dir == 'up':
+        board.move_up()
+    if dir == 'down':
+        board.move_down()
 
-    maxplayer = Agent('X')
-    minplayer = Agent('O')
-
-    while True:
-
-        (value, move) = maxplayer.maxvalue(game, minplayer)
-        game = game.neighbor(move, maxplayer.mark)
-        time.sleep(1)
-        game.display()
-        
-        if game.utility() is not None:
-            break
-        
-        (value, move) = minplayer.minvalue(game, maxplayer)
-        game = game.neighbor(move, minplayer.mark)
-        time.sleep(1)
-        game.display()
-        
-        if game.utility() is not None:
-            break
-
-if __name__ == '__main__':
-    main()
+print (board)
